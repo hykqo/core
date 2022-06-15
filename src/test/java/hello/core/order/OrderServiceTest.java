bd395a7e3fd5d7f4db2e342672af23eb050273d8
ENOCH YEOM <hykqo@naver.com>
2022-06-15 22:15:48 +0900

    주문기능, 할인기능 테스트

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,25 @@
+package hello.core.order;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderSerivceImpl();
+
+    @Test
+    void createOrder(){
+        // long 기본형 타입으로 사용해도 상관은 없으나 null값이 들어가지 않기때문에 Long타입으로 작성한다.
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+}
