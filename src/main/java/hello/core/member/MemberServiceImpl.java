3c32c40e85b099bfe19292eae536c54ad5948f4b
ENOCH YEOM <hykqo@naver.com>
2022-06-16 22:38:49 +0900

    appConfing도입으로 인한 MemberService 코드 변경

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,8 +1,14 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
+
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
@@ -11,6 +17,7 @@ public class MemberServiceImpl implements MemberService{
 
     @Override
     public Member findMember(Long memberId) {
+
         return memberRepository.findBtyId(memberId);
     }
 }
