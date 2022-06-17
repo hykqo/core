b8f3fc221b418e02b5a924bdbb123e8c6fdcbe2a
ENOCH YEOM <hykqo@naver.com>
2022-06-17 23:24:57 +0900

    FixDiscount -> RateDiscount로 구현객체 변경

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,7 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -22,7 +22,8 @@ public class AppConfig {
         return new OrderSerivceImpl(memberRepository(), discountPolicy());
     }
 
-    private FixDiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
