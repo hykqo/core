c6a7ea45f22238da890e7dacf882b6782e338156
ENOCH YEOM <hykqo@naver.com>
2022-09-01 18:20:53 +0900

    수동 빈 등록 vs 자동 빈 등록(에러 발생x - 스프링부트는 에러 발생)

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -15,5 +18,10 @@ import org.springframework.context.annotation.FilterType;
         //만약 지정하지 않는다면, @ComponentScan이 붙은 클래스의 패키지가 시작위치가 된다.
 )
 public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 
 }
