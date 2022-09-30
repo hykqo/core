86b556d97169eda6a7465f72b57b2a8c60caf980
ENOCH YEOM <hykqo@naver.com>
2022-09-30 17:56:04 +0900

    의존관계 자동주입 - 생성자 주입

--- src/main/java/hello/core/order/OrderSerivceImpl.java
+++ src/main/java/hello/core/order/OrderSerivceImpl.java
@@ -9,31 +9,28 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderSerivceImpl implements OrderService{
 
-     private  MemberRepository memberRepository;
-     private  DiscountPolicy discountPolicy;
+     private  final MemberRepository memberRepository;
+     private  final DiscountPolicy discountPolicy;
 
-//    @Autowired(required = false)
+//     @Autowired
 //    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-//        System.out.println("discountPolicy = " + discountPolicy);
 //        this.discountPolicy = discountPolicy;
 //    }
 //
 //    @Autowired
 //    public void setMemberRepository(MemberRepository memberRepository) {
-//        System.out.println("memberRepository = " + memberRepository);
 //        this.memberRepository = memberRepository;
 //    }
 
-    @Autowired
-    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy){
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-
-    }
+//    @Autowired
+//    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy){
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//
+//    }
 
     @Autowired
     public OrderSerivceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        System.out.println("OrderSerivceImpl");
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
